e881c09c455f7a778c560cb407e4f1f930f488ca
Ilyas-12345 <112771019+Ilyas-12345@users.noreply.github.com>
2022-11-10 00:49:30 +0300

    Add files via upload

---
+++ 2 zadanie s funct/Source.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include "arrays.h"
+int main()
+{
+    int** array;
+    int min, i, j, count, rows, columns, mine = 0, k;
+    printf("input rows=");
+    scanf_s("%d", &rows);
+    printf("input columns=");
+    scanf_s("%d", &columns);
+    array = (int**)malloc(rows * sizeof(int*));
+	input_array(array, rows, columns);
+	output_array(array, rows, columns);
+    printf("\n");
+	search_min(array, rows, columns);
+	output_newarray(array, rows);
+}
---
+++ 2 zadanie s funct/arrays.h
@@ -0,0 +1,7 @@
+#pragma once
+#ifndef arrays.h
+void input_array(int** array_1, int rows_1, int columns_1);
+void output_array(int** array_1, int rows_1, int columns_1);
+void search_min(int** array_1, int rows_1, int columns_1);
+void output_newarray(int** array_1, int rows_1);
+#endif 
