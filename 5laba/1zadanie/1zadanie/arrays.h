18cef592b728420833a275bfb805939e4b545f81
Ilyas-12345 <112771019+Ilyas-12345@users.noreply.github.com>
2022-11-14 23:33:02 +0300

    Add files via upload

---
+++ 5laba/1zadanie/1zadanie/arrays.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <stdio.h>  
+#include <stdlib.h>  
+
+void input_size(int* size_1);
+void input_array(int** array_1, int size_1);
+void output_array(int** array_1, int size_1);
+void counts(int** array_1, int* size_1);
+
