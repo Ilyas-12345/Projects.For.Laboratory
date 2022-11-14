1d028bf04738f1e6bf97ec517e134e727bcb886d
Ilyas-12345 <112771019+Ilyas-12345@users.noreply.github.com>
2022-11-15 01:02:31 +0300

    Add files via upload

---
+++ 5laba/2zadanie/2zadanie/arrays.c
@@ -0,0 +1,102 @@
+#include "arrays.h"
+
+void input_array_size(int* rows)
+{
+    printf("Enter the array row \n");
+    while (scanf_s("%d", rows) == 0 || *rows < 0 || getchar() != '\n')
+    {
+        printf("Wrong input.\n");
+        rewind(stdin);
+    }
+}
+
+void array_memory_allocate(int rows, int*** array)
+{
+    *array = (int**)malloc(rows, sizeof(int*));;
+    for (int i = 0; i < rows; i++)
+    {
+        (*array)[i] = (int*)calloc(1 , sizeof(int));
+    }
+}
+
+void input_array(int rows, int*** array)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        int j = 0;
+        while (printf("elem[%d][%d] = ", i, j), scanf_s("%d", &(*array)[i][j]) != 0 && (*array)[i][j] != 0)
+        {
+            (*array)[i] = (int*)realloc((*array)[i], (j + 2) * sizeof(int));
+            j++;
+        }
+    }
+}
+void output_array( int rows, int*** array)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        int j = 0;
+        while ((*array)[i][j] != 0)
+        {
+            printf("%d ", (*array)[i][j]);
+            j++;
+            if ((*array)[i][j] == 0)
+            {
+                printf("\n");
+            }
+        }
+    }
+}
+void search_min(int*** array_1, int columns_1, int i, int* min_1)
+{
+    
+    for (int j = 0; j < (columns_1); j++) {
+        if ((*min_1) > (*array_1)[i][j]) {
+            (*min_1) = (*array_1)[i][j];
+        }
+    }
+}
+void delete_min(int*** array_1, int rows_1)
+{
+    for (int i = 0; i < rows_1; i++) {
+        int columns_1 = 0;
+        int min_1 = (*array_1)[i][0];
+        array_find_length_of_the_col(i, &columns_1, array_1);
+        if (i % 2 == 0)
+        {
+            search_min(array_1, columns_1, i, &min_1);
+            for (int j = 0; j < columns_1; j++)
+            {
+                if ((*array_1)[i][j] == min_1)
+                {
+                    columns_1--;
+                    for (int k = j; k < columns_1; k++)
+                    {
+                        (*array_1)[i][k] = (*array_1)[i][k + 1];
+                    }
+                    j--;
+                    (*array_1)[i] = (int*)realloc((*array_1)[i], columns_1 * sizeof(int));
+                }
+            }
+        }
+        
+    }
+}
+void array_find_length_of_the_col( int i, int* col, int*** array)
+{
+    int j = 0;
+    while ((*array)[i][j])
+    {
+        (*col)++;
+        j++;
+    }
+}
+void free_array(int*** array_1, int rows_1)
+{
+    for (int i = 0; i < rows_1; i++)
+    {
+        free((*array_1)[i]);
+    }
+    free((*array_1));
+    return 0;
+}
---
+++ 5laba/2zadanie/2zadanie/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include "arrays.h"
+int main()
+{
+    int** array;
+    int rows;
+    input_array_size(&rows);
+    array_memory_allocate(rows, &array);
+    input_array(rows, &array);
+    output_array(rows,&array);
+    delete_min(&array, rows);
+    printf("\n");
+    output_array(rows, &array);
+    free_array(&array, rows);
+}
