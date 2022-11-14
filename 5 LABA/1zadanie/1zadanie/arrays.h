7d421bbb333617e083bf2ee91a078ff949527d4a
Ilyas-12345 <112771019+Ilyas-12345@users.noreply.github.com>
2022-11-14 23:32:22 +0300

    Delete 5 LABA/1zadanie directory

--- 5 LABA/1zadanie/1zadanie/arrays.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdio.h>  
-#include <stdlib.h>  
-
-void input_size(int* size_1);
-void input_array(int** array_1, int size_1);
-void output_array(int** array_1, int size_1);
-void counts(int** array_1, int* size_1);
-
