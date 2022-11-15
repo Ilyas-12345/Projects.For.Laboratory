f41ad1a85b1361178830cc53f1b792e20d188a5e
Ilyas-12345 <112771019+Ilyas-12345@users.noreply.github.com>
2022-11-15 19:29:36 +0300

    Delete 3 zadanie s funct directory

--- 3 zadanie s funct/arrays.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef arrays.h
-	void check_array(int* rows_1);
-	void check1_array(int* columns_1);
-	void input_array(int*** array_1, int rows_1, int columns_1);
-	void output_array(int*** array_1, int rows_1, int columns_1);
-	void array_shift(int*** array_1, int rows_1, int columns_1);
-	void output_newarray(int*** array_1, int rows_1, int columns_1);
-#endif
