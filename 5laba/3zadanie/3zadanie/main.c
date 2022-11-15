af5e1638abcbbe69617ffca1265a7f32038795a8
Ilyas-12345 <112771019+Ilyas-12345@users.noreply.github.com>
2022-11-15 09:44:42 +0300

    Add files via upload

---
+++ 5laba/3zadanie/3zadanie/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "arrays.h" 
+
+int main()
+{
+
+	int** array;
+	int i, j, rows, columns, count = 0;
+	check_array(&rows);
+	check1_array(&columns); 
+	memory_allocate(&array, rows, columns);
+	input_array(&array, rows, columns);
+	output_array(&array, rows, columns);
+	printf("new mass:\n");
+	array_shift(&array, rows, columns);
+	output_newarray(&array, rows, columns);
+	free_array(&array, rows);
+	return 0;
+}
