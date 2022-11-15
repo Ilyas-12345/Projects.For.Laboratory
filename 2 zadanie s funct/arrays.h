cffdac3012e54dd56c0527a73df792f8022f95e1
Ilyas-12345 <112771019+Ilyas-12345@users.noreply.github.com>
2022-11-15 19:29:29 +0300

    Delete 2 zadanie s funct directory

--- 2 zadanie s funct/arrays.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#ifndef arrays.h
-void input_Array(int** array_1, int rows_1, int columns_1);
-void output_array(int** array_1, int rows_1, int columns_1);
-void search_min(int** array_1, int rows_1, int columns_1);
-void output_newarray(int** array_1, int rows_1);
-#endif 
