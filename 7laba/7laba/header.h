25752b1c73043be801d57ead5c6efba4ec5e5b4a
Ilyas-12345 <112771019+Ilyas-12345@users.noreply.github.com>
2022-12-01 01:09:43 +0300

    Delete 7laba/7laba directory

--- 7laba/7laba/header.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-void allocateMemory(char** string_1, int size_1);
-void String(char** string_1, int size_1);
-void ChangeRegistår(char** string_1, int size_1);
-void outputString(char** string_1, int size_1);
